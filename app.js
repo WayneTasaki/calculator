e1172664ae87edc2787fc471e6d6fd4749b61cb3
Wayne Tasaki <waynetasaki@gmail.com>
2022-04-05 15:23:43 -0700

    super basic functionality added

--- app.js
+++ app.js
@@ -1,19 +1,69 @@
-function add(num1, num2) {
-  return num1 + num2;
-}
+let currentNum = ''
+let previousNum = ''
+let operator = ''
 
-function subtract(num1, num2) {
-  return num1 - num2;
-}
+const clearButton = document.querySelector('.clear-button');
+const deleteButton = document.querySelector('.delete-button');
+const numButtons = document.querySelectorAll('.number-button');
+const operators = document.querySelectorAll('.operator-button');
+const equals = document.querySelector('.equals-button')
+equals.addEventListener('click', operate)
 
-function multiply(num1, num2) {
-  return num1 * num2;
-}
+const currentNumDisplay = document.querySelector('.currentNumDisplay')
+const previousNumDisplay = document.querySelector('.previousNumDisplay');
 
-function divide(num1, num2) {
-  return num1 / num2;
+numButtons.forEach(btn => {
+  btn.addEventListener('click', e => {
+    displayNums(e.target.textContent)
+  })
+});
+// adds an event listener to each number button and fires the displayNums function
+
+function displayNums(number) {
+  if (currentNum.length <= 16) {
+  currentNum += number
+  currentNumDisplay.textContent = currentNum
+  }
 }
+// displays the current number in the calculator window and up to 16 characters
+
+operators.forEach(btn => {
+  btn.addEventListener('click', (e) => {
+    displayOperators(e.target.textContent)
+  })
+})
+// adds event listener to each operator button and fires the displayOperators function
+
+function displayOperators(op) {
+  operator = op
+  previousNum = currentNum
+  previousNumDisplay.textContent = `${previousNum} ${op}`
+  currentNum = ''
+  currentNumDisplay.textContent = ''
+}
+// since clicking an operator indicates another number will be selected, this changes the number displayed in the displayNums function to a variable called previousNum and displays that above the currentNum section along with the operator
 
 function operate() {
-
+  currentNum = Number(currentNum)
+  previousNum = Number(previousNum)
+  if (operator === '+') {
+    previousNum = previousNum + currentNum
+  } else if (operator === '−') {
+    previousNum = previousNum - currentNum
+  } else if (operator === '×') {
+    previousNum = previousNum * currentNum
+  } else if (operator === '÷') {
+      if (currentNum <= 0) {
+        previousNum = 'ERROR'
+        previousNumDisplay.textContent = ''
+        currentNumDisplay.textContent = previousNum
+        operator = ''
+        return
+      }
+      previousNum = previousNum / currentNum
+  }
+  previousNum = previousNum.toString()
+  previousNumDisplay.textContent = ''
+  currentNumDisplay.textContent = previousNum
 }
+// The main operator function that calculates and displays the results
